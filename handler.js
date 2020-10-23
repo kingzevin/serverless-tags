e5deb578d43653984ff0824f93ffda692838e145
King.Zevin <king.zevin@gmail.com>
2020-10-23 11:34:24 +0800

    v3.1 - added handler.js

--- handler.js
+++ handler.js
@@ -1,11 +1,11 @@
 'use strict'
 // environment variables specified by the user
-process.env["MONGO_CONNECTION_STRING"] = `mongodb://172.17.0.1:27017/sharelatex`; // tags.config.env
+process.env["MONGO_CONNECTION_STRING"] = `mongodb://172.17.0.1:27017/sharelatex`; // tags.config.envs
 // the user should specify the express listener
-const expressListener = require('./app.js') // tags.express.file
+const expressListener = require('./app.js') // tags.express.object
 
 const owServerlessExpress = require('./owServerlessExpress.js')
 
-exports.main = function(params){ // tags.handler.function
+exports.main = function(params){
   return owServerlessExpress(expressListener, params)
 }
